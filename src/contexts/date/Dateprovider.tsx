b772f8a1a242ee3acb689d50793686042868d65b
r-mckeith <r.mckeith@me.com>
2024-04-18 12:57:43 -0400

    default to today on app load

--- src/contexts/date/Dateprovider.tsx
+++ src/contexts/date/Dateprovider.tsx
@@ -1,5 +1,6 @@
-import React, { useState, ReactNode } from 'react';
+import React, { useState, useEffect, ReactNode } from 'react';
 import DateContext from './DateContext';
+import { AppState, AppStateStatus } from 'react-native';
 
 type DateProviderProps = {
   children: ReactNode;
@@ -7,6 +8,27 @@ type DateProviderProps = {
 
 const DateProvider = ({ children }: DateProviderProps) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+
+  useEffect(() => {
+    const setToday = () => {
+      const today = new Date();
+      setSelectedDate(new Date(today.getFullYear(), today.getMonth(), today.getDate()));
+    };
+
+    setToday();
+
+    const handleAppStateChange = (nextAppState: AppStateStatus) => {
+      if (nextAppState === 'active') {
+        setToday();
+      }
+    };
+
+    const appStateSubscription = AppState.addEventListener('change', handleAppStateChange);
+
+    return () => {
+      appStateSubscription.remove();
+    };
+  }, []);
 
   return (
     <DateContext.Provider value={{ selectedDate, setSelectedDate }}>
@@ -16,3 +38,5 @@ const DateProvider = ({ children }: DateProviderProps) => {
 };
 
 export default DateProvider;
+
+
