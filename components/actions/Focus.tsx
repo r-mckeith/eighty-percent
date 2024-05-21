4795dab852eae931c5ae4a2fb46ffd95f3a9dfa9
r-mckeith <r.mckeith@me.com>
2024-05-21 09:58:36 -0400

    sticky indices

--- components/actions/Focus.tsx
+++ components/actions/Focus.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import { useColorScheme, Text } from 'react-native';
 import { getColors } from '../../src/colors';
-import { useReviewContext } from '../../src/contexts';
 import { Section, Row } from '../layout';
 
-export default function Focus() {
-  const { reviews } = useReviewContext();
+export default function Focus({ review }: { review: string }) {
 
   const scheme = useColorScheme();
   const colors = getColors(scheme);
-
-  const lastReview = reviews && reviews[0]?.response ? reviews[0]?.response : null;
-
-  if (!lastReview) return
 
   return (
     <Section>
       <Row opacity={1} first={true} last={true}>
         <Text>
           <Text style={[{ fontWeight: 'bold' }, colors.text]}>This week's focus: </Text>
-          <Text style={colors.text}>{lastReview && lastReview.improve}</Text>
+          <Text style={colors.text}>{review}</Text>
         </Text>
       </Row>
     </Section>
--- screens/Actions.tsx
+++ screens/Actions.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { ActivityIndicator, View, StyleSheet, useColorScheme } from 'react-native';
-import { useDateContext, useHabitContext, useGroupContext, usePlanContext, useTaskContext } from '../src/contexts';
+import {
+  useDateContext,
+  useGroupContext,
+  useHabitContext,
+  usePlanContext,
+  useReviewContext,
+  useTaskContext,
+} from '../src/contexts';
 import { getColors } from '../src/colors';
 import { ReviewButton, AddButton } from '../components/shared';
 import { Scroll, SectionTitle } from '../components/layout';
@@ -12,6 +19,7 @@ export default function Habits() {
   const { groups } = useGroupContext();
   const { habits } = useHabitContext();
   const { plans } = usePlanContext();
+  const { reviews } = useReviewContext();
   const { tasks } = useTaskContext();
 
   const scheme = useColorScheme();
@@ -24,7 +32,8 @@ export default function Habits() {
   const groupId = habitGroup && habitGroup[0]?.id;
   const planSection = filterPlans(plans, selectedDateString);
   const habitSection = habits.filter(habit => habit.section === 'habits');
-  const stickyIndices = planSection.length > 0 ? [2, 4] : [3];
+  const lastReview = reviews && reviews[0]?.response ? reviews[0]?.response : null;
+
 
   function filterPlans(plans: any, selectedDateString: string) {
     return plans.filter((plan: any) => {
@@ -43,18 +52,48 @@ export default function Habits() {
     );
   }
 
+  function renderFocus() {
+    if (lastReview) {
+      return <Focus review={lastReview.improve} />;
+    }
+  }
+
+  function renderPlanSectionTitle() {
+    if (planSection.length > 0) {
+      return <SectionTitle title={planSection.length > 0 || weeklyReview ? 'Plans' : ''} />;
+    }
+  }
+
+  function renderPlanSection() {
+    if (planSection.length > 0) {
+      return (
+        <PlanSection plans={planSection} hasReview={weeklyReview}>
+          <ReviewButtonRow first={planSection.length === 0 ? true : false} />
+        </PlanSection>
+      );
+    }
+  }
+
+  function getStickyIndices() {
+    if (lastReview && planSection.length > 0) {
+      return [2, 4];
+    } else if (lastReview && planSection.length === 0) {
+      return [2];
+    } else if (!lastReview && planSection.length > 0) {
+      return [1, 3];
+    } else if (!lastReview && planSection.length === 0) {
+      return [1];
+    }
+  }
+
   return (
-    <Scroll stickyIndices={stickyIndices}>
+    <Scroll stickyIndices={getStickyIndices()}>
       <View style={colors.background}>
         <DateSelector selectedDate={selectedDate} onDateChange={setSelectedDate} />
       </View>
-      <Focus />
-      <SectionTitle title={planSection.length > 0 || weeklyReview ? 'Plans' : ''} />
-      {planSection.length > 0 && (
-        <PlanSection plans={planSection} hasReview={weeklyReview}>
-          <ReviewButtonRow first={planSection.length === 0 ? true : false}/>
-        </PlanSection>
-      )}
+      {renderFocus()}
+      {renderPlanSectionTitle()}
+      {renderPlanSection()}
 
       <SectionTitle title={'Habits'}>
         {<AddButton sectionName={'habits'} type={'habit'} groupId={groupId} />}
